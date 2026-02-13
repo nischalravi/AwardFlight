ce6760411de283bb348261f73330f4534f525e0a
Nischal Ravichandran <n.ravichandran@northeastern.edu>
2026-02-12 19:54:41 -0500

    Replace with clean working files - no duplicates

--- search-flights-data.js
+++
@@ -1,6 +0,0 @@
-
-window.flightDatabase = flightDatabase;
-console.log('✅ Flights:', flightDatabase.length);
-
-window.flightDatabase = flightDatabase;
-console.log('✅ Flights:', flightDatabase.length);
