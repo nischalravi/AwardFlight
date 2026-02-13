589ab4810bb05fbb1e1e0f235a429b413e9283f0
Nischal Ravichandran <n.ravichandran@northeastern.edu>
2026-02-12 19:47:20 -0500

    Add exports and auto-render

--- search-flights-data.js
+++ search-flights-data.js
@@ -1,3 +1,6 @@
 
 window.flightDatabase = flightDatabase;
 console.log('✅ Flights:', flightDatabase.length);
+
+window.flightDatabase = flightDatabase;
+console.log('✅ Flights:', flightDatabase.length);
